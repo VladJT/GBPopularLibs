7599baac9f21e3a8147921ef588cdb1113a0dd48
Богданков Владислав <onlyrus@hotmail.com>
2022-11-16 20:44:32 +0300

    refactor

--- app/src/main/java/jt/projects/gbpopularlibs/ui/UsersFragment.kt
+++ app/src/main/java/jt/projects/gbpopularlibs/ui/UsersFragment.kt
@@ -4,13 +4,18 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.DefaultItemAnimator
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import jt.projects.gbpopularlibs.App
+import jt.projects.gbpopularlibs.R
 import jt.projects.gbpopularlibs.databinding.FragmentUsersBinding
 import jt.projects.gbpopularlibs.model.GithubUsersRepositoryLocalImpl
 import jt.projects.gbpopularlibs.presenter.UsersPresenter
 import jt.projects.gbpopularlibs.ui.interfaces.BackButtonListener
 import jt.projects.gbpopularlibs.ui.interfaces.UsersView
+import jt.projects.gbpopularlibs.utils.DURATION_ITEM_ANIMATOR
 import moxy.MvpAppCompatFragment
 import moxy.ktx.moxyPresenter
 
@@ -40,9 +45,22 @@ class UsersFragment : MvpAppCompatFragment(), UsersView, BackButtonListener {
 
     override fun init() {
         adapter = UsersRVAdapter(presenter.usersListPresenter)
-        binding?.rvUsers?.let {
-            it.layoutManager = LinearLayoutManager(context)
-            it.adapter = adapter
+        binding?.rvUsers?.let { rv->
+            rv.layoutManager = LinearLayoutManager(context)
+            rv.adapter = adapter
+
+//            DividerItemDecoration(requireContext(), GridLayoutManager.HORIZONTAL).also {
+//                it.setDrawable(resources.getDrawable(R.drawable.separator, null))
+//                rv.addItemDecoration(it)
+//            }
+
+            // Установим анимацию. А чтобы было хорошо заметно, сделаем анимацию долгой
+            rv.itemAnimator = DefaultItemAnimator().apply {
+                addDuration = DURATION_ITEM_ANIMATOR
+                changeDuration = DURATION_ITEM_ANIMATOR
+                removeDuration = DURATION_ITEM_ANIMATOR
+                moveDuration = DURATION_ITEM_ANIMATOR
+            }
         }
     }
 
