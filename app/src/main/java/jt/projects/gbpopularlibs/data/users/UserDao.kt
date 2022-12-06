2eb49d26857a8bfa5e83210c8d56421e2ae0c3ec
VladJT <onlyrus@hotmail.com>
2022-12-06 17:39:37 +0300

    lection 6 start

---
+++ app/src/main/java/jt/projects/gbpopularlibs/data/users/UserDao.kt
@@ -0,0 +1,27 @@
+package jt.projects.gbpopularlibs.data.users
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.Query
+import jt.projects.gbpopularlibs.domain.entities.UserEntity
+
+@Dao
+interface UserDao {
+
+    @Query("Select * from UserEntity")
+    fun getAll():List<UserEntity>
+
+    @Query("select * from UserEntity where id in (:uids)")
+    fun getAllById(uids: IntArray): List<UserEntity>
+
+    @Query("SELECT * FROM UserEntity WHERE login LIKE :first LIMIT 1")
+    fun findByName(first: String, last: String): UserEntity
+
+    @Insert
+    fun insertAll(vararg users: UserEntity)
+
+    @Delete
+    fun delete(user: UserEntity)
+
+}
